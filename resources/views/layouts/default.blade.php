54b66dcfbd5cfccfe227ae16f1c278fa21b14500
hmlihui <1214589895@qq.com>
2020-04-24 06:48:43 +0000

    完成用户注册功能s

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,29 +1,19 @@
 <!DOCTYPE html>
 <html>
   <head>
-
-    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-
-    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
-
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
-  <body>
-  	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  		<div class="container">
-  			<a class="navbar-brand" href="/">weibo App</a>
-  			<ul class="navbar-nav justify-content-end">
-  				<li class="nav-item">
-  					<a class="nav-link" href="/help">帮助</a>
-  				</li>
-  				<li>
-  					<a class="nav-link" href="#">登录</a>
-  				</li>
-  			</ul>
-  		</div>
-  	</nav>
-  	<div class="container">
-  		   @yield('content')
-  	</div>
 
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
   </body>
 </html>
