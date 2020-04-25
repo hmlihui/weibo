867f3d91d6be5d25b5464c03ab370f83be9fe595
hmlihui <1214589895@qq.com>
2020-04-25 07:21:07 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,13 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         $users = factory(User::class)->times(50)->make();
@@ -17,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
