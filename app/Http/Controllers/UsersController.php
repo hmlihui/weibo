d3e8fb0561b72df2277c694befee7a46d942658e
hmlihui <1214589895@qq.com>
2020-04-27 02:53:26 +0000

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,25 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Mail;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Mail;
 
 class UsersController extends Controller
 {
     public function __construct()
     {
-       $this->middleware('auth', [
-            'except' => ['show', 'create', 'store','index','confirmEmail']
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail'],
         ]);
 
         $this->middleware('guest', [
-            'only' => ['create']
+            'only' => ['create'],
         ]);
     }
-       public function index()
+    public function index()
     {
-         $users = User::paginate(10);
+        $users = User::paginate(10);
         return view('users.index', compact('users'));
     }
 
@@ -47,13 +47,13 @@ class UsersController extends Controller
             'email'    => $request->email,
             'password' => bcrypt($request->password),
         ]);
-      $this->sendEmailConfirmationTo($user);
+        $this->sendEmailConfirmationTo($user);
         session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
         return redirect('/');
     }
 
     public function edit(User $user)
-    {   
+    {
         $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
@@ -78,7 +78,7 @@ class UsersController extends Controller
         return redirect()->route('users.show', $user);
     }
 
-      public function destroy(User $user)
+    public function destroy(User $user)
     {
         $this->authorize('destroy', $user);
         $user->delete();
@@ -86,24 +86,22 @@ class UsersController extends Controller
         return back();
     }
 
-      protected function sendEmailConfirmationTo($user)
+    protected function sendEmailConfirmationTo($user)
     {
-        $view = 'emails.confirm';
-        $data = compact('user');
-        $from = 'summer@example.com';
-        $name = 'Summer';
-        $to = $user->email;
+        $view    = 'emails.confirm';
+        $data    = compact('user');
+        $to      = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
-       public function confirmEmail($token)
+    public function confirmEmail($token)
     {
         $user = User::where('activation_token', $token)->firstOrFail();
 
-        $user->activated = true;
+        $user->activated        = true;
         $user->activation_token = null;
         $user->save();
 
